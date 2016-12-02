37e28cde7e5fd548adb173a7bf699ea8ab0e7b68
githublu <yilu331@gmail.com>
2016-12-01 21:40:20 -0800

    add

---
+++ webapi/protheta/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace protheta.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            List<string> users = new List<string>();
+            using (MySqlDB db = new MySqlDB())
+            {
+                if (db.Connect())
+                {
+                    string sql = "SELECT * FROM tbl_users";
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.CommandText = sql;
+                    cmd.Connection = db.Connection();
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        users.Add(dataReader["name"].ToString());
+                    }
+                }
+
+                return users;
+            }
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+        private void MySQLdbConnection()
+        {
+            using (MySqlDB db = new MySqlDB())
+            {
+                if (db.Connect())
+                {
+                    string sql = "INSERT INTO `tbl_user` (`mc_userName`, `mc_userPass`, `tw_userName`, `tw_userPass`) VALUES (@mcUserName, @mcUserPass, @twUserName, @twUserPass)";
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddWithValue("@mcUserName", "1");
+                    cmd.Parameters.AddWithValue("@mcUserPass", "1");
+                    cmd.Parameters.AddWithValue("@twUserName", "1");
+                    cmd.Parameters.AddWithValue("@twUserPass", "1");
+                    cmd.Connection = db.Connection();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+    }
+}
---
+++ webapi/protheta/Providers/MySqlDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace protheta
+{
+    public class MySqlDB : IDisposable
+    {
+        private MySqlConnection connection = null;
+
+        public MySqlDB()
+        {
+
+        }
+
+        public MySqlConnection Connection()
+        {
+            return connection;
+        }
+
+        public bool Connect()
+        {
+            bool isConnected = true;
+
+            string connstring = string.Format("Server=localhost; database=project1; UID=project1; password=project1");
+
+            if (connection == null)
+            {
+                connection = new MySqlConnection(connstring);
+                try
+                {
+                    connection.Open();
+                    isConnected = true;
+                }
+                catch
+                {
+                    connection.Close();
+                    isConnected = false;
+                }
+            }
+
+            return isConnected;
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+            connection = null;
+        }
+    }
+}
